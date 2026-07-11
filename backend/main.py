ac65091b4a969ee41175a0ee87a9cb6999606510
James Humphrey <jhumphrey.candu@gmail.com>
2026-07-11 15:01:12 -0400

    Add int type to item_id

--- backend/main.py
+++ backend/main.py
@@ -4,8 +4,10 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello WOrld"}
+    return {"message": "Hello World!"}
 
 @app.get("/items/{item_id}")
-async def read_item(item_id):
+async def read_item(item_id: int):
     return {"item_id":item_id}
+
+
