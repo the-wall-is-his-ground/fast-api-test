c9dcd4b5ffc0229481e0ade382905e4c60912ac9
James Humphrey <jhumphrey.candu@gmail.com>
2026-07-14 20:50:19 -0400

    Add items query and path parameter

--- backend/main.py
+++ backend/main.py
@@ -7,16 +7,28 @@ class ModelName(str, Enum):
     resnet = "resnet"
     lenet = "lenet" 
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id":item_id}
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
 
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q : str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q":q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 @app.get("/users/me")
 async def read_user_me():
     return {"user_id": "the current user"}
